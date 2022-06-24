a412475ca0e24254f52c8bdd737fcb19e7f97dbd
Moisés do Nascimento <moises.d.nasci@gmail.com>
2022-06-24 16:38:17 -0300

    init

---
+++ src/main/java/dio/controller/ClienteRestController.java
@@ -0,0 +1,24 @@
+package dio.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dio.model.Cliente;
+import dio.service.ClienteService;
+
+@RestController
+@RequestMapping("clientes")
+public class ClienteRestController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Cliente>> buscarTodos() {
+        return ResponseEntity.ok(clienteService.buscarTodos());
+    }
+
+}
