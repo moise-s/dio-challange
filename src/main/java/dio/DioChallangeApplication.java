c470eea6604724d4b020443935405ada017736be
Moisés do Nascimento <moises.d.nasci@gmail.com>
2022-06-24 16:38:32 -0300

    init

---
+++ src/main/java/dio/DioChallangeApplication.java
@@ -0,0 +1,21 @@
+package dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@EnableFeignClients
+@SpringBootApplication
+public class DioChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioChallangeApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello World";
+	}
+
+}
---
+++ src/main/java/dio/service/ClienteService.java
@@ -0,0 +1,17 @@
+package dio.service;
+
+import dio.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
