3bf6c2e2e4ff53f39fbec59edcbc9f06f825a724
Moisés do Nascimento <moises.d.nasci@gmail.com>
2022-06-30 18:04:59 -0300

    updates

--- src/main/java/dio/controller/ClienteRestController.java
+++ src/main/java/dio/controller/ClienteRestController.java
@@ -2,12 +2,17 @@ package dio.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dio.model.Cliente;
 import dio.service.ClienteService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("clientes")
@@ -19,6 +24,29 @@ public class ClienteRestController {
     @GetMapping
     public ResponseEntity<Iterable<Cliente>> buscarTodos() {
         return ResponseEntity.ok(clienteService.buscarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(clienteService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente) {
+        clienteService.inserir(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+        clienteService.atualizar(id, cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        clienteService.deletar(id);
+        return ResponseEntity.ok().build();
     }
 
 }
---
+++ src/main/java/dio/controller/ViaCepService.java
@@ -0,0 +1,16 @@
+package dio.controller;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import dio.model.Endereco;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+    @GetMapping("/{cep}/json/")
+    static Endereco consultarCep(@PathVariable("cep") String cep) {
+        // TODO Auto-generated method stub
+        return cep;
+    }
+}
